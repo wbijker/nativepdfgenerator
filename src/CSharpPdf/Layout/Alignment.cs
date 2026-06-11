d4c8a5f2e538c338a710a126bd8f3bac62a66d2d
Willem Bijker <willem@bijker.co.za>
2026-06-11 09:26:25 +0200

    Remove CSharpPDf

--- src/CSharpPdf/Layout/Alignment.cs
+++
@@ -1,7 +0,0 @@
-namespace CSharpPdf.Layout;
-
-/// <summary>How a component is positioned horizontally within wider available space.</summary>
-public enum HorizontalAlignment { Left, Center, Right }
-
-/// <summary>How a component is positioned vertically within taller available space (e.g. a row cell).</summary>
-public enum VerticalAlignment { Top, Middle, Bottom }
